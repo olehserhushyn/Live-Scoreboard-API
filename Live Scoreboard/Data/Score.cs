fbba4b8b0a3f7331a4016bdafae242ad8f9d9907
Oleh <olehserhushyn.job@gmail.com>
2025-11-08 22:47:38 +0000

    initial

---
+++ Live Scoreboard/Data/Score.cs
@@ -0,0 +1,11 @@
+﻿namespace Live_Scoreboard.Data
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public string TeamOneName { get; set; }
+        public string TeamTwoName { get; set; }
+        public int TeamOneScore { get; set; }
+        public int TeamTwoScore { get; set; }
+    }
+}
---
+++ Live Scoreboard/Hubs/ScoreHub.cs
@@ -0,0 +1,13 @@
+﻿using Live_Scoreboard.Data;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Live_Scoreboard.Hubs
+{
+    public class ScoreHub : Hub
+    {
+        public async Task SendScores(List<Score> scores)
+        {
+            await Clients.All.SendCoreAsync("ReceiveScores", new object[] { scores });
+        }
+    }
+}
